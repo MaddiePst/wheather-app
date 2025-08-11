98fe44a64d708625b6a3f85651603c2536ad6f11
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2025-08-11 14:27:17 -0400

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,29 +3,10 @@ import SearchBar from "./SearchBar";
 // import SearchResultsList from "./SearchResultsList";
 
 export default function Header(props) {
-  // const [results, setResults] = useState([]);
-  // const [input, setInput] = useState("");
-
-  // const fetchData = (value) => {
-  //   fetch(
-  //     "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={67263f61c34be6002f3dec2554277cb1}"
-  //   ).then(
-  //     (response) => response.json().then((json) => console.log(json))
-  //     //   {
-  //     //   const results = json.filter((location) => {
-  //     //     return (
-  //     //       value && location && location.name.toLowerCase().includes(value)
-  //     //     );
-  //     //   });
-  //     //   setResults(results);
-  //     // })
-  //   );
-  // };
-
-  // const handleChange = (value) => {
-  //   fetchData(value);
-  //   setInput(value);
-  // };
+  function getCurLocation() {
+    props.getLocation();
+    props.search(props.location.latitude, props.location.longitude);
+  }
 
   return (
     <header className="header">
@@ -44,8 +25,8 @@ export default function Header(props) {
           <label className="site-search"></label>
           <SearchBar
             city={props.city}
-            setCity={props.setCity}
-            handleChange={props.handleChange}
+            onKeyDown={props.onKeyDown}
+            onChange={props.onChange}
           />
           {/* <SearchResultsList 
             city={props.city}
@@ -54,7 +35,7 @@ export default function Header(props) {
         </div>
       </form>
       {/* Current Location */}
-      <button className="location">
+      <button className="location" onClick={() => getCurLocation()}>
         <ion-icon name="locate" className="icon"></ion-icon>
         Current Location
       </button>
--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,16 +1,16 @@
 // import { useState } from "react";
 
+// I want to be able to search the city and display automatically
 export default function SearchBar(props) {
-  // Still need to work on the fetch website
-
   return (
     <div className="input-wrapper">
       <input
         // type="search"
         className="search-bar"
         placeholder="🔎Search for your preffered city..."
-        value={props.input}
-        onChange={(e) => props.handleChange(e.target.value)}
+        value={props.city}
+        onKeyDown={props.onKeyDown}
+        onChange={props.onChange}
       />
     </div>
   );
