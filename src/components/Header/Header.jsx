3638b99260d70841f683aebf0bf9bdc4c82da7fa
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2025-08-13 15:01:21 -0400

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,11 @@
 import SearchBar from "./SearchBar";
 // import SearchResultsList from "./SearchResultsList";
 
-export default function Header(props) {
-  function getCurLocation() {
-    props.getLocation();
-    props.search(props.location.latitude, props.location.longitude);
-  }
+export default function Header({ city, onChange, onKeyDown, getLocation }) {
+  // function getCurLocation() {
+  //   props.getLocation();
+  //   // props.search(props.location.latitude, props.location.longitude);
+  // }
 
   return (
     <header className="header">
@@ -14,7 +14,7 @@ export default function Header(props) {
       <div className="toggle-container">
         <label className="toggle-label">
           <input type="checkbox" className="toggle-input" />
-          <span className="slider" onClick={props.toggleDarkMode}></span>
+          {/* <span className="slider" onClick={props.toggleDarkMode}></span> */}
         </label>
         <p>Dark Mode</p>
       </div>
@@ -23,11 +23,7 @@ export default function Header(props) {
       <form action="/search" method="get" className="form-container">
         <div className="serarch-bar-container">
           <label className="site-search"></label>
-          <SearchBar
-            city={props.city}
-            onKeyDown={props.onKeyDown}
-            onChange={props.onChange}
-          />
+          <SearchBar city={city} onKeyDown={onKeyDown} onChange={onChange} />
           {/* <SearchResultsList 
             city={props.city}
             handleChange={props.handleChange}
@@ -35,7 +31,7 @@ export default function Header(props) {
         </div>
       </form>
       {/* Current Location */}
-      <button className="location" onClick={() => getCurLocation()}>
+      <button className="location" onClick={() => getLocation()}>
         <ion-icon name="locate" className="icon"></ion-icon>
         Current Location
       </button>
--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,16 +1,16 @@
 // import { useState } from "react";
 
 // I want to be able to search the city and display automatically
-export default function SearchBar(props) {
+export default function SearchBar({ city, onChange, onKeyDown }) {
   return (
     <div className="input-wrapper">
       <input
         // type="search"
         className="search-bar"
         placeholder="🔎Search for your preffered city..."
-        value={props.city}
-        onKeyDown={props.onKeyDown}
-        onChange={props.onChange}
+        value={city}
+        onKeyDown={onKeyDown}
+        onChange={onChange}
       />
     </div>
   );
