67492d77350eb6ddf7f396415f6ab91ab37306f6
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2025-08-16 16:40:53 -0400

    Delete src/components/HourlyForecastSection.jsx

--- src/components/HourlyForecastSection.jsx
+++
@@ -1,7 +0,0 @@
-export default function HourlyForecastSection() {
-  return (
-    <div className="hourly-forecast-container">
-      <h2 className="title-center">Hourly Forecast:</h2>
-    </div>
-  );
-}
